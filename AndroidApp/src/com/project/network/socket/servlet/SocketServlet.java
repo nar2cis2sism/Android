8a74e5ca85227c37a2035ab71575edcf891ae5d6
unknown <yWX349077@DGGG5YWX3490771.china.huawei.com>
2018-05-08 10:48:17 +0800

    socket单机

--- AndroidApp/src/com/project/network/socket/servlet/SocketServlet.java
+++ AndroidApp/src/com/project/network/socket/servlet/SocketServlet.java
@@ -1,15 +1,16 @@
 package com.project.network.socket.servlet;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import engine.android.core.util.LogFactory.LOG;
-import engine.android.framework.util.GsonUtil;
-import engine.android.util.io.IOUtil;
 import protocol.java.ProtocolWrapper;
 import protocol.java.ProtocolWrapper.ProtocolEntity;
 import protocol.java.ProtocolWrapper.ProtocolEntity.ProtocolData;
+import protocol.java.stream.ack.MessageACK;
+import protocol.java.stream.req.Message;
+import protocol.java.stream.req.Message.MessageBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SocketServlet implements engine.android.socket.SocketProxy.SocketServlet {
 
@@ -24,7 +25,7 @@ public class SocketServlet implements engine.android.socket.SocketProxy.SocketSe
 
             entity.parseBody();
             
-            ProtocolData[] ack = process(entity.getCmd(), entity.getData());
+            ProtocolData[] ack = process(entity.getCmd(), entity.getData(), out);
             if (ack != null)
             {
                 for (ProtocolData d : ack)
@@ -39,26 +40,50 @@ public class SocketServlet implements engine.android.socket.SocketProxy.SocketSe
         }
     }
     
-    private ProtocolData[] process(int cmd, ProtocolData data) throws Exception {
-        Class<?> cls = data.getClass();
-        String name = cls.getSimpleName();
-        String json = new String(getFileContent(name));
-        return (ProtocolData[]) GsonUtil.parseJson(json, cls);
-    
+    /**
+     * 模拟服务器处理
+     * 
+     * @param cmd 收包指令码
+     * @param data 收包数据
+     * @param out 推送给客户端
+     * @return 响应客户端
+     */
+    private ProtocolData[] process(int cmd, ProtocolData data, OutputStream out) throws Exception {
+        if (data instanceof Message)
+        {
+            return processMessage((Message) data, out);
+        }
+        
+        return null;
     }
     
-    private static byte[] getFileContent(String fileName) throws IOException {
-        InputStream is = null;
-        try {
-            is = SocketServlet.class.getResourceAsStream(fileName);
-            if (is == null)
+    /**
+     * 推送消息给客户端
+     */
+    private void push(OutputStream out, ProtocolData data) throws Exception {
+        ProtocolEntity entity = ProtocolEntity.newInstance(0, data);
+        entity.generateBody();
+        out.write(ProtocolWrapper.wrap(entity));
+    }
+    
+    private ProtocolData[] processMessage(Message msg, OutputStream out) throws Exception {
+        // 消息返还给发送者
+        String receiver = msg.to;
+        msg.to = msg.from;
+        msg.from = receiver;
+        // 重新设置时间
+        MessageBody[] body = msg.body;
+        if (body != null)
+        {
+            for (MessageBody b : body)
             {
-                throw new IOException("No resource:" + fileName);
+                b.creationTime = System.currentTimeMillis();
+                b.content = "收到：" + b.content;
             }
-            
-            return IOUtil.readStream(is);
-        } finally {
-            if (is != null) is.close();
         }
+        
+        push(out, msg);
+        
+        return new ProtocolData[]{ new MessageACK() };
     }
 }

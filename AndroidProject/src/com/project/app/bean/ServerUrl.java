bfef0ba236994fbcfa11c1bd4520b97ae8edda66
nar2cis2sism <nar2cis2sism@163.com>
2019-09-24 15:35:53 +0800

    代码整理

--- AndroidProject/src/com/project/app/bean/ServerUrl.java
+++
@@ -1,8 +0,0 @@
-package com.project.app.bean;
-
-public class ServerUrl {
-    
-    public String socket_server_url;        // Socket服务器地址
-    public String upload_server_url;        // 文件上传服务器地址
-    public String download_server_url;      // 文件下载服务器地址
-}
